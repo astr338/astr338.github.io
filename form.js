421dec56acd2ec1bee74091b7f4a9d5719c7bdbf
rmncbr <romanjacob.cabrera@icloud.com>
2022-11-28 01:40:02 -0800

    Updates

--- form.js
+++
@@ -1,8 +0,0 @@
-function openForm(String) {
-  return (String === 'open-button' ? document.getElementById("myForm").style.display = "block" : document.getElementById("bib").style.display = "block");
-}
-
-function closeForm(String) {
-  return (String === 'close-button' ? document.getElementById("myForm").style.display = "none" : document.getElementById("bib").style.display = "none");
-}
-
